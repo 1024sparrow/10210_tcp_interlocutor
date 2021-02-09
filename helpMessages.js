e95d2a6246ddeb1fbff3da7837dee2d06b7263cb
1024sparrow <1024sparrow@gmail.com>
2021-02-09 20:17:29 +0300

    123

---
+++ helpMessages.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+module.exports = {
+	general: `
+************************
+* q - quit
+* h - instant help
+* space - pause/resume
+* PgUp/PgDn - quick jump to the previous/next paragraph respectively
+* backspace - stop and show whole book's contents
+* s - begin setting sound settings up
+*   u - speed 40%
+*   i - speed 80%
+*   o - speed 100%
+*   p - speed 120%
+*   j - shift 3s
+*   k - shift 1s
+*   l - null shift
+*   Esc - quit sound setting settings mode
+* 
+
+* Alt + 1 - highlighting for sound
+* Alt + 2 - highlighting for translation
+
+movings in time forward and backward
+
+j - join with previous paragraph
+s - split. And replay current chunk.
+
+
+
+************************
+`
+};
--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
 const readline = require('readline');
+const helpMessages = require('./helpMessages.js');
 
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
 
 let state = 0;
 for (const arg of process.argv.slice(2)){
@@ -18,10 +15,30 @@ Author: boris.
 	//else if (arg === '--')
 }
 
-var command;
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key)=>{
+	if ((key.ctrl && key.name === 'c') || key.name === 'q'){
+		process.stdout.write('\n');
+		process.exit();
+	}
+	if (key.name === 'h') {
+		process.stdout.write(helpMessages.general);
+	}
+	else if (key.name === '1'){
+		process.stdout.write('1111\n');
+	}
+});
+
+
+//var command;
 //while (command !== 'q'){
 //	command = readline
 //}
-rl.on('line', (p_line)=>{
-	console.log(`(${p_line})`);
-});
+//const rl = readline.createInterface({
+//	input: process.stdin,
+//	output: process.stdout
+//});
+//rl.on('line', (p_line)=>{
+//	console.log(`line(${p_line})`);
+//});
